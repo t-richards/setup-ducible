fe8df81609a3efc1da629569440f7d01f2232269
Tom Richards <tom@tomrichards.net>
2020-01-30 00:13:20 -0500

    Download the stuff

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,47 @@
 import * as core from "@actions/core";
-import { wait } from "./wait";
+import * as tc from "@actions/tool-cache";
+import * as path from "path";
+
+const DUCIBLE = "ducible";
+const DUCIBLE_VERSION = "1.2.2";
+const ARCH = "x64";
+
+export function ducibleUrl(): string {
+  return `https://github.com/jasonwhite/ducible/releases/download/v${DUCIBLE_VERSION}/ducible-windows-${ARCH}-Release.zip`;
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput("milliseconds");
-    core.debug(`Waiting ${ms} milliseconds ...`);
+    // Attempt to find cached version of the tool
+    const cacheDir = await tc.find(DUCIBLE, DUCIBLE_VERSION);
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    // Add cached tool to path
+    if (cacheDir) {
+      core.debug(`Found ${DUCIBLE} v${DUCIBLE_VERSION} in cache ${cacheDir}`);
 
-    core.setOutput("time", new Date().toTimeString());
+      const ducibleExe = path.join(cacheDir, "ducible.exe");
+      await core.addPath(ducibleExe);
+
+      return;
+    }
+
+    // Download fresh release zip
+    const url: string = ducibleUrl();
+    core.debug(`Downloading ${url} ...`);
+    const downloadPath = await tc.downloadTool(url);
+    core.debug(downloadPath);
+
+    // Unzip the downloaded file
+    const zipFolder = await tc.extractZip(downloadPath);
+    core.debug(zipFolder);
+
+    // Place tool in the cache
+    const newCacheDir = await tc.cacheDir(zipFolder, DUCIBLE, DUCIBLE_VERSION);
+    core.debug(newCacheDir);
+
+    // Add tool to the path
+    const ducibleExe = path.join(newCacheDir, "ducible.exe");
+    await core.addPath(ducibleExe);
   } catch (error) {
     core.setFailed(error.message);
   }
